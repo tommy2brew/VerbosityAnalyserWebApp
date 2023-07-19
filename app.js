dc87245e48d4c7cf65390e18b78f1feeb79a74b0
tommy2brew <tompoulter6@gmail.com>
2023-07-19 16:44:13 +0100

    authorisation flow first implementation completed, getting and refreshing of access tokens works

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const QueryString = require("qs");
+const querystring = require("querystring");
+const axios = require('axios');
+const https = require('https');
+require('dotenv').config();
 const app = express();
 const port = 3001;
 
@@ -8,35 +11,135 @@ app.use(express.static('views'));
 
 //handling routes//
 app.get('/', (req, res) => {
-    res.sendFile('wordify.html', {root: './views'});
-})
+    res.sendFile('wordify.html', { root: './views' });
+});
 
 app.get('/about', (req, res) => {
-    res.sendFile('about.html', {root: './views'})
-})
+    res.sendFile('about.html', { root: './views' })
+});
 
 app.get('/privacy', (req, res) => {
-    res.sendFile('privacy.html', {root: './views'})
-})
+    res.sendFile('privacy.html', { root: './views' })
+});
 
-//start of API routes
+//API routes
 const clientID = "ac87cd645d534f7e83940acbba480fb3";
+const secret = "794fd75ed48f439f90a3da71038094b3";
+const redirectURI = 'http://localhost:3001/callback';
+const authorisation = 'Basic ' + Buffer.from(clientID + ':' + secret).toString('base64');
+const contentType = 'application/x-www-form-urlencoded';
 
-app.get('/login', function(req, res){
+//sending user to spotify login//
+app.get('/login', function (req, res) {
     var authURL = "https://accounts.spotify.com/authorize?";
     res.redirect(authURL +
-        QueryString.stringify({
+        querystring.stringify({
             client_id: clientID,
             response_type: "code",
-            redirect_uri: 'http://localhost:3001',
+            redirect_uri: redirectURI,
             scope: "user-top-read",
-            show_dialog: "false"
+            show_dialog: "true"
         }));
 });
 
-//document.getElementById("login").addEventListener("click", requestAuth);
+let accessToken = "";
+let refreshToken = "";
+
+//gettting api tokens after user authenticates and sending user to score page//
+app.get('/callback', function (req, res) {
+    console.log("callbacked");
+    if (req.query.error) {
+        res.sendFile('wordify.html', { root: './views' });
+        console.log(req.query.error);
+        return;
+    };
+    let authCode = req.query.code;
+
+    let options = {
+        method: 'POST',
+        hostname: 'accounts.spotify.com',
+        path: '/api/token',
+        headers: {
+            'Authorization': authorisation,
+            'Content-Type': contentType
+        }
+    };
+
+    let postBody = querystring.stringify({
+        grant_type: 'authorization_code',
+        code: authCode,
+        redirect_uri: redirectURI
+    });
+
+    let post = https.request(options, (outcome) => {
+        let response = '';
+
+        outcome.on('data', (chunk) => {
+            response += chunk;
+        });
+
+        outcome.on('end', () => {
+            if (outcome.statusCode === 200) {
+                response = JSON.parse(response);
+                console.log(response);
+                accessToken = response.access_token;
+                refreshToken = response.refresh_token;
+                res.sendFile('score.html', { root: './views' });
+            }
+            else {
+                console.log(outcome.statusCode);
+            };
+        });
+    });
+    post.on('error', (error) => {
+        console.error(error);
+    });
+    post.write(postBody);
+    post.end();
+});
+
+//refreshing the access token when it expires// 
+app.get('refresh_token', function(req, res){
+    let refresh_token = req.query.refresh_token;
+    let options = {
+        method: 'POST',
+        hostname: 'accounts.spotify.com',
+        path: '/api/token',
+        headers: {
+            'Authorization' : authorisation,
+            'Content-Type' : contentType
+        }
+    };
+
+    let postBody = querystring.stringify({
+        grant_type : 'refresh_token',
+        refresh_token : refresh_token
+    });
+
+    let post = https.request(options, (outcome) =>{
+        let response = '';
+
+        outcome.on('data', (chunk) =>{
+            response += chunk;
+        });
+
+        outcome.on('end', () => {
+            if(outcome.statusCode === 200) {
+                response = JSON.parse(response);
+                console.log(response);
+                accessToken = response.access_token;
+            };
+        });
+    })
+
+    post.on('error', (error) => {
+        console.error(error);
+    });
+    post.write(postBody);
+    post.end();
+});
 
 //starting the server
 app.listen(port, () => {
-    console.log("Hello I have startedddd");
+    console.log("Hello I have started");
 });
